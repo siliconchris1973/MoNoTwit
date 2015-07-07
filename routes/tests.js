c6bf863fc85b1e8a43146076cb0bdb86b6e71dc5
Christian Guenther <chris.guenther@mac.com>
2015-07-07 10:30:46 +0200

    added test resource

--- routes/tests.js
+++ routes/tests.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/test', function(req, res, next) {
+/* GET test listing. */
+router.get('/', function(req, res, next) {
   res.send('respond with a test resource');
 });
 
